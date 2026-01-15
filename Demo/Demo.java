88af4b8e2571d787e6d38aca7542ca4986b8d263
divyansh-bhardwaj-01 <divyanshb240@gmail.com>
2026-01-16 00:14:43 +0530

    updated

---
+++ Demo/Demo.java
@@ -0,0 +1,10 @@
+public class Demo2{
+public static void main(string[] args){
+System.out.println("Helloworld");
+System.out.println("Hello Ishant");
+System.out.println("Hello web");
+System.out.println("Hello web3");
+
+}
+
+} 
