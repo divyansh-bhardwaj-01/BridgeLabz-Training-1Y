0eca2b5cf6ca2644f2b76adfd0d75d5b36e4d694
divyansh-bhardwaj-01 <divyanshb240@gmail.com>
2026-01-19 23:39:54 +0530

    distribute pens

---
+++ basic-programs/Primitive-Data-Type/Level1/Pendistribute.java
@@ -0,0 +1,10 @@
+public class Pendistribute{
+public static void main(String[] args){
+int penquan=14;
+int student=3;
+int nondistripen=14%3;
+int distripen=14/3;
+
+System.out.println("The pen per student is " +distripen+ " and the remaining pen not distributed is "+nondistripen);
+}
+}
