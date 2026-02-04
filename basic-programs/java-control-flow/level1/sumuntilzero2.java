0b07b6f5ab11c3de466282ed83e6a062d2355aa9
divyansh-bhardwaj-01 <divyanshb240@gmail.com>
2026-02-04 22:17:36 +0530

    find the sum of numbers until the user enters 0 .

---
+++ basic-programs/java-control-flow/level1/sumuntilzero2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class sumuntilzero2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double total = 0.0;
+
+        while (true) {
+
+            double num = sc.nextDouble();
+
+            if (num <= 0) {
+                break;
+            }
+
+            total = total + num;
+        }
+
+        System.out.println(total);
+
+        sc.close();
+    }
+}
