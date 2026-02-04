d7c6693310d38ec5e39bf38d7393c00ed535c213
divyansh-bhardwaj-01 <divyanshb240@gmail.com>
2026-02-04 23:02:49 +0530

    Leap Year with single if condition

---
+++ basic-programs/java-control-flow/level3/Leapyear2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Leapyear2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if (year >= 1582) {
+
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                System.out.println("Leap Year");
+            } 
+            else {
+                System.out.println("Not a Leap Year");
+            }
+
+        } 
+        else {
+            System.out.println("Year must be 1582 or above");
+        }
+    }
+}
